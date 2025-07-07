41f39c84728c5dc932b60b4a0bb7dd101bc06574
dongyeon <ldy1951123@gmail.com>
2025-07-07 23:43:55 +0900

    배포 타이틀 이미지 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="icon" type="image/png" href="/nallijaku.png" />
+      </head>
       <body>{children}</body>
     </html>
   );
