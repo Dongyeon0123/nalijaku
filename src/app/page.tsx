52d99662d0789cd1a5123e7676d16ddfa39f1741
Dongyeon0123 <165901822+Dongyeon0123@users.noreply.github.com>
2025-08-21 04:15:43 +0900

    모바일에서 요소 가운데정렬

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,6 +124,17 @@ export default function Home() {
           #how {
             background-image: none !important;
             background-color: #ffffff !important;
+            display: flex !important;
+            align-items: center !important;
+            justify-content: center !important;
+            text-align: center !important;
+          }
+          
+          #why {
+            display: flex !important;
+            align-items: center !important;
+            justify-content: center !important;
+            text-align: center !important;
           }
         }
 
