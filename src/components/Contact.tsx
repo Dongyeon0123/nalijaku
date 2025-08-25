624c361bb523db864c037ce3b0105728f98f6738
dongyeon <ldy1951123@gmail.com>
2025-08-25 22:25:00 +0900

    contact colorsection부분 개발

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
-import { IoChevronForward } from 'react-icons/io5';
+import { IoChevronForward, IoLogoInstagram, IoLogoYoutube, IoBusiness } from 'react-icons/io5';
 import styles from '@/styles/Contact.module.css';
 
 export default function Contact() {
@@ -30,7 +30,61 @@ export default function Contact() {
           </div>
         </div>
       </div>
-      <div className={styles.colorSection}></div>
+      <div className={styles.colorSection}>
+        <div className={styles.footerContent}>
+          {/* 왼쪽: 로고 */}
+          <div className={styles.logoSection}>
+            <Image 
+              src="/transparentLogo.png" 
+              alt="날리자쿠 로고" 
+              width={120} 
+              height={60}
+            />
+          </div>
+          
+          {/* 중앙: 회사 정보 */}
+          <div className={styles.companyInfo}>
+            <h3 className={styles.companyName}>날리자쿠</h3>
+            <div className={styles.infoList}>
+              <p><span>대표</span>|  이민상</p>
+              <p><span>사업자 등록번호</span>|  215-65-00727</p>
+              <p><span>연락처</span>|  010.5029.6452</p>
+              <p><span>주소</span>|  충청북도 청주시 서원구 서원서로 30-23<br /><br></br>SK 하이닉스 창업관</p>
+            </div>
+            <div className={styles.legalLinks}>
+              <a href="#" className={styles.legalLink}>서비스 이용약관</a>
+              <a href="#" className={styles.legalLink}>개인정보처리방침</a>
+            </div>
+          </div>
+          
+          {/* 오른쪽: 메뉴 */}
+          <div className={styles.menuSection}>
+            <h4 className={styles.menuTitle}>메뉴</h4>
+            <div className={styles.menuList}>
+              <a href="#" className={styles.menuLink}>학습자료</a>
+              <a href="#" className={styles.menuLink}>커뮤니티</a>
+              <a href="#" className={styles.menuLink}>날리자쿠 소개</a>
+              <a href="#" className={styles.menuLink}>사용 가이드</a>
+            </div>
+          </div>
+          
+          {/* SNS 섹션 */}
+          <div className={styles.snsSection}>
+            <h4 className={styles.snsTitle}>날리자쿠 SNS</h4>
+            <div className={styles.snsIcons}>
+              <a href="#" className={styles.snsIcon}>
+                <IoLogoInstagram size={24} />
+              </a>
+              <a href="#" className={styles.snsIcon}>
+                <IoLogoYoutube size={24} />
+              </a>
+              <a href="#" className={styles.snsIcon}>
+                <IoBusiness size={24} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
