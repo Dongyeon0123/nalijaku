9a05011472d24098a98f25ecfa210feb20675940
dongyeon <ldy1951123@gmail.com>
2025-08-26 15:47:19 +0900

    네이버&구글 인증 메타 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko">
       <head>
         <link rel="icon" type="image/png" href="/logo.png" />
+        <meta name="naver-site-verification" content="5d07fbec5c7214a2aa549c9e290eea204ceab2e6" />
+        <meta name="google-site-verification" content="N6nZQYHXxDVkRfgMM85UNZHbMYCnCKz9sqKtDMl6sCc" />
       </head>
       <body>
         {children}
