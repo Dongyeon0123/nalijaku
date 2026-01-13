5599057b37f487975b9473705c596adafaacbbd7
dongyeon <ldy1951123@gmail.com>
2026-01-13 15:45:37 +0900

    구글 애널리틱스 스니펫 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -166,8 +166,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="naver-site-verification" content="5d07fbec5c7214a2aa549c9e290eea204ceab2e6" />
         <meta name="google-site-verification" content="N6nZQYHXxDVkRfgMM85UNZHbMYCnCKz9sqKtDMl6sCc" />
         <link rel="alternate" type="application/rss+xml" title="날리자쿠 외부소식 RSS" href="/rss.xml" />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WHQW9NDD');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WHQW9NDD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
         <CollaborationSidebar />
       </body>
